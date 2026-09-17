6ef0a5907861f0586e537b376372ceca8e37201b
mayankkakkar2327 <mayankkakkar23@gmail.com>
2026-09-17 10:06:29 +0530

    Keep earlier CAT news articles alongside deadline update

--- data/site.js
+++ data/site.js
@@ -11,6 +11,7 @@ if (!global.__catPreparationBlogPagesPatch) {
         require("./blog-rodha-cracku-pages"),
         require("./blog-extra-pages"),
         require("./blog-cat-deadline-pages"),
+        require("./blog-news-pages").filter((page) => page.slug !== "blog"),
         require("./rodha-ecosystem-pages").filter((page) => page.slug.startsWith("cat-coaching/rodha/"))
       );
     }
